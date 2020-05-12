b4a520cb8bffb1225331828ff560f26590fc35e2
Brandon Patton <bpatton@stevens.edu>
2020-05-12 16:07:16 -0400

    help

--- public/javascript/helper.js
+++
@@ -1,8 +0,0 @@
-const Handlebars = require("handlebars");
-Handlebars.registerHelper('ifCond', function(op, options) {
-    if(op != "") {
-        return options.fn(this)
-    }else{
-      return options.inverse(this)
-    }
-  });
