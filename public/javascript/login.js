76f0b2492729c1f7ac21c481e7cebd7a40f0d5dd
Ryan Qin <rqin@stevens.edu>
2020-05-13 17:55:27 -0400

    Update user error handling

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -11,20 +11,6 @@ $('#myForm').submit((event) => {
     }
 });
 
-$('#updateForm').submit((event) => {
-    let password_regular_expression = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    if(!password_regular_expression.test($('#password').val())){
-        showError('password','Password length must be between 6 to 15 characters long and contain at least one special character, one number, and one letter');
-    } else if(!Object.keys($('#u-genres').val()).length || $('#u-genres').val().length > 3 ){
-        showError('u-genres','You must choose up to 3 favorite genres.');
-    } else if (!Object.keys($('#u-artists').val()).length || $('#u-artists').val().length > 3) {
-        showError('u-artists','You must choose up to 3 favorite artists.');
-    } else {
-        $('#error').hide(); 
-    }
-});
-
-
 function showError(focusField,message){
     $('#error').show();
     $('#error').html(message);
